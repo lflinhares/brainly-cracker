eb2ee2eb6eb63d98ba11eb8c77e5e5b06dbc34e2
luiz linhares <luiz.linhares@agenciam3.com>
2023-12-09 12:08:17 -0300

    feat: clear browser localStorage and cookies

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,18 @@
 async function initializeExtension() {
   console.log("Extension initialized");
+
+  function clearBrowser() {
+    window.localStorage.clear();
+    document.cookie = "";
+  }
+
+  clearBrowser();
+  const interval = setInterval(clearBrowser, 500);
+
+  setTimeout(() => {
+    clearInterval(interval);
+    console.log("Extension cleared");
+  }, 5000);
 }
 
 if (typeof extensionInit === "undefined") {
